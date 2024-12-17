7e23a785a07ccedba2833698846f6ae1cbce491f
Joost van Bruggen <joostvbruggen@gmail.com>
2024-12-17 12:10:29 +0100

    small test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          You Learn React Today, Today I Say!
         </a>
       </header>
     </div>
